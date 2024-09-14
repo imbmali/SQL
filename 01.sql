d57d09f4648ab7d5c40f822357e202a908986d20
imbmali <geeky.bhavesh@gmail.com>
2024-09-14 15:08:25 +0530

    initial commit

---
+++ 01.sql
@@ -0,0 +1,9 @@
+create TABLE student (
+  id int PRIMARY key,
+  name varchar(255)
+  );
+  
+  insert into student VALUES(1, 'Bhavesh')
+  
+  select * from student;
+  
---
+++ 02.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Companies (
+  id int,
+  name varchar(50),
+  address text,
+  email varchar(50),
+  phone varchar(10)
+);
+
+insert into Companies VALUES (1, 'Bhavesh', 'HP', 'geeky@b.com', '7814884914');
+SELECT * FROM Companies;
+
